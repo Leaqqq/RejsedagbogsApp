79ae5b011fb1be5ac0f890a4ff67e825fc9e83c0
Leaq <frederikgrummesgaard@gmail.com>
2019-04-25 10:10:38 +0200

    Layout næsten færdigt
    Mangler onClicks på buttons og at hooke de forskellige notes og travels ind på listerne og også oprettelse.

--- RejsedagbogApp/app/src/main/java/com/example/rejsedagbogapp/Activities/TravelsActivity.java
+++ RejsedagbogApp/app/src/main/java/com/example/rejsedagbogapp/Activities/TravelsActivity.java
@@ -1,4 +1,11 @@
 package com.example.rejsedagbogapp.Activities;
 
+import android.os.Bundle;
+
+import com.example.rejsedagbogapp.R;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class TravelsActivity {
+
+
 }
